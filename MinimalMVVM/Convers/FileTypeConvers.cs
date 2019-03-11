8c321c45d7863989578799d42f250ed59a7a852e
GangDang <42027227+GangDang@users.noreply.github.com>
2019-03-11 17:35:12 +0800

    Ini

---
+++ MinimalMVVM/Convers/FileTypeConvers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace MinimalMVVM.Convers
+{
+    public class FileTypeConvers : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((string)value).ToLowerInvariant();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MinimalMVVM/Views/ConverterControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MinimalMVVM.Views
+{
+    public partial class ConverterControl
+    {
+        public static readonly RoutedCommand Open = new RoutedCommand("Open", typeof(ConverterControl));
+        public static readonly RoutedCommand DetailInfo = new RoutedCommand("DetailInfo", typeof(ConverterControl));
+        public ConverterControl()
+        {
+            InitializeComponent();
+            CommandBindings.Add(new CommandBinding(Open, delegate (object obj, ExecutedRoutedEventArgs e) { MessageBox.Show("open"); }));
+            CommandBindings.Add(new CommandBinding(DetailInfo, delegate (object obj, ExecutedRoutedEventArgs e) { MessageBox.Show("Info"); }));
+
+            FileInfo[] fileInfos = new DirectoryInfo(@"C:\Users\zhangdafeng\Desktop\ObjectAttachFile\").GetFiles("*.*");
+            this.DataFileName.ItemsSource = fileInfos;
+        }
+
+    }
+}
